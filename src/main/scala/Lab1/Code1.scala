59e2d677498ff198603ccc95aec256da3312436c
s15309 <s15309@pjwstk.edu.pl>
2020-10-14 18:05:48 +0200

    Wstępne rozwiązanie 2

--- src/main/scala/Lab1/Code1.scala
+++ src/main/scala/Lab1/Code1.scala
@@ -5,16 +5,20 @@ object Code1 {
     println("Zadanie 2")
     println("---------")
 
-    var listaDni : Array[String] = ("Poniedzialek" ,"Wtorek" , "Sroda" , "Czwartek" , "Piatek" , "Sobota" , "Niedziela")
+    var listaDni = List("Poniedzialek" ,"Wtorek" , "Sroda" , "Czwartek" , "Piatek" , "Sobota" , "Niedziela")
 
 
-    def wypiszTab(lista: Array[String] , size: Int , idx: Int): String ={
+    def wypiszTab(lista: List[String] , indeks: Int = 0): Unit ={
       var slowo: String = ""
-      if(size >= idx){
-        return slowo
+      if(indeks == 0) {
+        slowo = lista(indeks)
+      }else {
+        slowo = ", " + lista(indeks)
       }
-
-    }: Unit
-
+        slowo = slowo + wypiszTab(lista , indeks+1)
+        return  slowo
+    }
+    var cos = wypiszTab(listaDni , 0 )
+    println(cos)
   }
 }
