cf84dff2126ef156f596541a7e8ad972266981ab
s15309 <s15309@pjwstk.edu.pl>
2020-10-14 14:38:57 +0200

    Zajecia PJATK

--- src/main/scala/Lab1/Code1.scala
+++ src/main/scala/Lab1/Code1.scala
@@ -1,5 +1,20 @@
 package Lab1
 
 object Code1 {
+  def main(args : Array[String]){
+    println("Zadanie 2")
+    println("---------")
 
+    var listaDni : Array[String] = ("Poniedzialek" ,"Wtorek" , "Sroda" , "Czwartek" , "Piatek" , "Sobota" , "Niedziela")
+
+
+    def wypiszTab(lista: Array[String] , size: Int , idx: Int): String ={
+      var slowo: String = ""
+      if(size >= idx){
+        return slowo
+      }
+
+    }: Unit
+
+  }
 }
